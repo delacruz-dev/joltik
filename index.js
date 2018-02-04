a871ef358a8779553d035d327e3a615dae630399
danderu <mail@dandel.es>
2018-02-04 07:06:45 +0100

    style(prettier): Install and configure prettier

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
 export function y(type, props, ...args) {
   const children = args.length ? [].concat(...args) : null;
   return {
-      type,
-      props,
-      children
-  }
+    type,
+    props,
+    children
+  };
 }
 
 function setProp(node, name, value) {
-  if (name === 'className') {
-      name = 'class'
+  if (name === "className") {
+    name = "class";
   }
 
   node.setAttribute(name, value);
 }
 
-function setProps (node, props = {}) {
+function setProps(node, props = {}) {
   if (!props) {
-    return
+    return;
   }
-  Object.keys(props).forEach(name => setProp(node, name, props[name]))
+  Object.keys(props).forEach(name => setProp(node, name, props[name]));
 }
 
 export function createElement(node) {
-  if (typeof node === 'string') {
-    return document.createTextNode(node)
-  }
-  
-  if (typeof node.type === 'object') {
-    return createElement(node.type)
+  if (typeof node === "string") {
+    return document.createTextNode(node);
   }
 
-  const element = document.createElement(node.type)
-  setProps(element, node.props)
-  node.children
-    .map(createElement)
-    .forEach(child => element.appendChild(child))
-  return element
+  if (typeof node.type === "object") {
+    return createElement(node.type);
+  }
+
+  const element = document.createElement(node.type);
+  setProps(element, node.props);
+  node.children.map(createElement).forEach(child => element.appendChild(child));
+  return element;
 }
